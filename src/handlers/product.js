97b8aa33bad6d453c00e3fd13d6d0db70df946be
Francisbene Mayer <fmmcba1@gmail.com>
2024-01-07 18:47:18 -0300

    Criação função transforme em handlers

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -1,8 +1,21 @@
 const productModel = require('../modules/product');
-
-const getAll = (request, h) => {
-    return 'Hello World!';
-}
+//Criando função transformer 
+const transformer = product => ({
+    type: 'products',
+    id: product.id,
+    attributes: {
+      name: product.name,
+      price: product.price,
+    },
+    links: {
+      self: `/api/v1/products/${product.id}`
+    }
+  });
+  
+ const getAll = async () => {
+    const products = await ProductRepository.getAll();
+    return { data: products.map(transformer) };
+ };
 
 const save = async (req, h) => {
     const {name, price} = req.payload;
