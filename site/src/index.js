af5e8052683e5b6f22a9aa58e3b9eaeeb0cd1f77
Francisbene Mayer <fmmcba1@gmail.com>
2024-01-08 20:57:10 -0300

    inclindo css

--- site/src/index.js
+++ site/src/index.js
@@ -6,6 +6,8 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
+app.use(express.static('public'));
+
 app.get('/produtos', async function(req, res) {
     const result = await request.get('http://localhost:3000/api/v1/products')
     const products = JSON.parse(result).data;
