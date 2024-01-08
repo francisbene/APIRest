ca790cee7977f6fcb36a53dc74aadff3513ad47a
Francisbene Mayer <fmmcba1@gmail.com>
2024-01-07 21:26:04 -0300

    incluir pasta views e arquivo products

--- site/src/index.js
+++ site/src/index.js
@@ -3,6 +3,9 @@ const request = require('request-promise-native');
 
 const app = express()
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.get('/produtos', async function(req, res) {
     const result = await request.get('http://localhost:3000/api/v1/products')
     console.log(JSON.parse(result).data);
